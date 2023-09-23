a974d37242ef2283e326fcb9394c70f128390481
Nakaya <eniehack@outlook.jp>
2023-09-23 19:08:59 +0900

    chore: build setting

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			pages: "build",
+			assets: "build",
+			fallback: undefined,
+			strict: true,
+		}),
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH
 		}
