a56b388dc37cb5038974933a912ed2b827449fc8
Nakaya <eniehack@outlook.jp>
2023-09-23 21:17:57 +0900

    style: format

--- svelte.config.js
+++ svelte.config.js
@@ -14,10 +14,10 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			pages: "build",
-			assets: "build",
+			pages: 'build',
+			assets: 'build',
 			fallback: undefined,
-			strict: true,
+			strict: true
 		}),
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH
