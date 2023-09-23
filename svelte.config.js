f81fb500b1a27ed3d34930aeca2b0626e7de2d45
Nakaya <eniehack@outlook.jp>
2023-09-23 19:01:08 +0900

    chore: github actions

--- svelte.config.js
+++ svelte.config.js
@@ -15,7 +15,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: dev ? "" : "/scrapbox-planetarium",
+			base: dev ? "" : process.env.BASE_PATH,
 		}
 	}
 };
