f52a7e90a47399be31f8a9c59ee3940b71925e84
Nakaya <eniehack@outlook.jp>
2023-09-25 12:59:11 +0900

    refactor: BaseModal

---
+++ src/lib/BaseModal.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    export let showModal: boolean;
+
+    let dialog: HTMLDialogElement;
+
+    $: if (dialog && showModal) dialog.showModal();
+</script>
+
+<dialog
+    bind:this={dialog}
+    on:close={() => showModal = false}
+    on:click|self={() => dialog.close()}>
+    <div>
+        <div class="header"></div>
+        <slot />
+        <div class="footer">
+            <button on:click={() => dialog.close()}>close</button>
+        </div>
+    </div>
+</dialog>
--- src/routes/FileSelector.svelte
+++ src/routes/FileSelector.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	import { schema } from '$lib/load';
+	import BaseModal from '$lib/BaseModal.svelte';
+import { schema } from '$lib/load';
 	import { file as fileStore } from '$lib/store';
 
 	let dialog: HTMLDialogElement;
 	let file: FileList;
+	let target_project: string;
 	export let showModal: boolean;
 
 	$: if (file) {
@@ -21,7 +23,15 @@
 	$: if (dialog && showModal) dialog.showModal();
 </script>
 
-<dialog bind:this={dialog}>
+<BaseModal bind:showModal>
+	<label for="project">対象とするproject</label>
+	<input name="project" type="text" bind:value={target_project}>
+	{#if target_project !== ""}
+		<p>
+			<a href={`https://scrapbox.io/projects/${target_project}/settings/backup`} target="_blank" rel="noreferrer noopener">ここ</a>
+			を開いてJSONを入手します
+		</p>
+	{/if}
 	<input type="file" id="file_elem" accept=".json" bind:files={file} />
 	<!-- TODO: dnd でファイル追加できてほし～～ -->
-</dialog>
+</BaseModal>
