c3211644514ee90a982eeb146cc7184acf1e92eb
Nakaya <eniehack@outlook.jp>
2025-09-07 19:58:21 +0900

    feat: メニューの要素をスクロールできるように etc.
    - feat: メニューの要素をスクロールできるように
    - refactor: 頻出の型をNode型に起こした
    - refactor: event handlerを関数に
    - feat: a11yの警告をなくした

--- src/routes/NodeDetailedMenu.svelte
+++ src/routes/NodeDetailedMenu.svelte
@@ -6,8 +6,12 @@
 		isOpen: boolean
 		graph: Graph
 	}
+	type Node = {
+		label: string,
+		node: string
+	}
 	let { node: selectedNode = $bindable(), graph, isOpen = $bindable() }: Props = $props();
-	let outNeighbors = $derived.by<Array<{label: string, node: string}>>(() => {
+	let outNeighbors = $derived.by<Array<Node>>(() => {
 		try {
 			return graph.outNeighbors(selectedNode).map(node => {
 				const label = graph.getNodeAttribute(node, "label") as string;
@@ -17,7 +21,7 @@
 			return []
 		}
 	})
-	let inNeighbors = $derived.by<Array<{label: string, node: string}>>(() => {
+	let inNeighbors = $derived.by<Array<Node>>(() => {
 		try {
 			const neighbours = graph.inNeighbors(selectedNode).map(node => {
 				const label = graph.getNodeAttribute(node, "label") as string;
@@ -33,34 +37,47 @@
 	const closePanel = () => {
 		isOpen = false;
 	}
+
+	const onNodeClicked = (neighbor: Node) => {
+		selectedNode = neighbor.node;
+	}
 </script>
 
-<div class="absolute top-0 left-0 w-80 h-full bg-white border-r border-gray-300 shadow-lg z-10">
-	<div class="p-6">
-
-	<button 
-		onclick={closePanel}
-		class="float-right text-gray-500 hover:text-gray-700 text-xl font-bold"
-	>
-		×
-	</button>
+<div class="absolute top-0 left-0 w-80 h-full bg-white border-r border-gray-300 shadow-lg z-10 flex flex-col" role="menu">
+	<div class="p-6 flex-shrink-0">
+		<button 
+			onclick={closePanel}
+			class="float-right text-gray-500 hover:text-gray-700 text-xl font-bold"
+		>
+			×
+		</button>
+	</div>
 	
-	<section class="prose">
+	<section class="prose flex-1 overflow-y-auto px-6 pb-6" >
 	<h2>{selectednodeLabel}</h2>
-	<div class="space-y-2 overflow-y-scroll">
-		<p>このページからリンクしているページたち</p>
-		<ul>
-		{#each outNeighbors as neighbor}
-			<li class="text-sky-600 hover:underline" onclick={() => selectedNode = neighbor.node}>{neighbor.label}</li> 
-		{/each}
-		</ul>
-		<p>このページへリンクしているページたち</p>
-		<ul>
-		{#each inNeighbors as neighbor}
-			<li class="text-sky-600 hover:underline" onclick={() => selectedNode = neighbor.node}>{neighbor.label}</li> 
-		{/each}
-		</ul>
+	<div class="space-y-2 overflow-scroll">
+		<section>
+			<h3>このページからリンクしているページたち</h3>
+			<ul>
+			{#each outNeighbors as neighbor}
+				<li class="text-sky-600 hover:underline">
+					<button type="button" class="inline-block cursor-pointer" onclick={() => onNodeClicked(neighbor)}>
+						{neighbor.label}
+					</button>
+				</li> 
+			{/each}
+			</ul>
+		</section>
+		<section>
+			<h3>このページへリンクしているページたち</h3>
+			<ul>
+			{#each inNeighbors as neighbor}
+				<li class="text-sky-600 hover:underline">
+					<button type="button" class="inline-block cursor-pointer" onclick={() => onNodeClicked(neighbor)}>{neighbor.label}</button>
+				</li> 
+			{/each}
+			</ul>
+		</section>
 	</div>
 	</section>
-	</div>
 </div>
